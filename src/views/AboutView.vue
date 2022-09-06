c4fc9fb583d8b10ad033e00bb20b57d4d9abc35f
frasfras <65541080+frasfras@users.noreply.github.com>
2022-09-07 00:23:07 +0800

    Add files via upload

---
+++ src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1 align="center">Feed</h1>
+    <!-- <iframe
+      src="https://frans-77.fred.sensetecnic.com/api/monitor"
+      width="1000"
+      height="800"
+    />-->
+    <PlanView />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import PlanView from "@/components/PlanView.vue";
+//import VolunteerOpp from "@/components/VolunteerOpp.vue";
+//import FeedOpp from "@/components/FeedOpp.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    PlanView,
+  },
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <HelloWorld msg="Click for Visual editor" />
+</template>
+
+<script>
+import HelloWorld from "../components/FeedOpp";
+
+export default {
+  name: "HomeView",
+
+  components: {
+    HelloWorld,
+  },
+};
+</script>
